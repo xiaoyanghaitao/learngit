38b1b743195503ec60b9817a8b6832a523f0ee6f
xiaoyanghaitao <1341040663@qq.com>
2015-06-01 22:58:11 +0800

    Second commit

--- src/com/example/broadbestpractice/LoginActivity.java
+++ src/com/example/broadbestpractice/LoginActivity.java
@@ -1,27 +1,43 @@
 package com.example.broadbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
 
+	private SharedPreferences pref;
+	private SharedPreferences.Editor editor;
 	private EditText accountEdit;
 	private EditText passwordEdit;
 	private Button login;
+	private CheckBox remeberPass;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		pref = PreferenceManager.getDefaultSharedPreferences(this);
 		accountEdit = (EditText) findViewById(R.id.account);
 		passwordEdit = (EditText) findViewById(R.id.password);
+		remeberPass = (CheckBox) findViewById(R.id.remeber_pass);
 		login = (Button) findViewById(R.id.login);
+		boolean isRemeber = pref.getBoolean("remeber_password", false);
+		if(isRemeber){
+			String account = pref.getString("account", "");
+			String password = pref.getString("password", "");
+			accountEdit.setText(account);
+			passwordEdit.setText(password);
+			remeberPass.setChecked(true);
+		}
 		login.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -30,6 +46,15 @@ public class LoginActivity extends BaseActivity {
 				String account = accountEdit.getText().toString();
 				String password = passwordEdit.getText().toString();
 				if (account.equals("admin") && password.equals("123456")) {
+					editor = pref.edit();
+					if(remeberPass.isChecked()){
+						editor.putBoolean("remeber_password", true);
+						editor.putString("account", account);
+						editor.putString("password", password);
+					} else {
+						editor.clear();
+					}
+					editor.commit();
 					Intent intent = new Intent(LoginActivity.this,
 							MainActivity.class);
 					startActivity(intent);
